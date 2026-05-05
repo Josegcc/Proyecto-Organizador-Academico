1f253798be4c210e8790119ebb2e5569b62a6e75
Josegcc <josegc356@gmail.com>
2026-05-04 20:05:30 -0400

    Version 0.1 del Proecyto Organizador Academico

--- crearhorario.c
+++ crearhorario.c
@@ -1,105 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ARRAY_LENGTH(x) (sizeof(x)/sizeof((x)[0]))
-#define ARRAY_COLS(x) (sizeof(x)[0]/sizeof((x)[0][0]))
 
-static char hora[15][20] = {0};
-static char materias[7][12][20] = {0};
-
-void formatearArchivo(){
-
-        int counter1=0;
-
-        for (int i = 0; i < ARRAY_LENGTH(hora); i++){
-        // Check if string is not NULL and not empty (length > 0)
-            if (hora[i] != NULL && strlen(hora[i]) > 0){
-                counter1++;
-                                                     }
-                                                }
-        //printf("Horas nos vacias: %d", counter1);
+void formatearArchivo(const char *nombre_arch, const char materias[7][12][50], const char hora[7][20], size_t tam_mat){
 
         FILE *archivo;
-        archivo = fopen("horarios.csv", "w");
+        archivo = fopen(nombre_arch, "w");
 
         if (archivo == NULL) {
-        perror("ERROR: The file is not opened.");
+            perror("ERROR: The file is not opened.");
+            return;
                              }
 
-        fprintf(archivo, "Horario,Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo\n");
+        fprintf(archivo, "Horario,Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo"); //MUESTRA LOS DIAS DE LA SEMANA (PRIMERA FILA)
 
         //PROBLEMA AL ESCRIBIR MAS DE 7
         for(int i = 0; i < 7; i++){
             fprintf(archivo, "%s,",hora[i]);
+            int counter1 = 0;
 
-            for(int j = 0; j < ARRAY_COLS(materias); j++){
+            for (int j = 0; j < tam_mat; j++){
+                if (hora[i][j] == NULL || !(strlen(hora[i]) > 0)){
+                counter1++;         //Almacena cuantas celdas llenas hay, cuantos espacios ocupados tiene el arreglo
+                                                                 }
+                                             }
+
+            for(int j = 0; j <= counter1; j++){
                 fprintf(archivo, "%s,",materias[j][i]);
-                                                         }
+                                              }
             fprintf(archivo,"\n");
+            counter1 = 0;
                                    }
-
-        fclose(archivo);
+      fclose(archivo);
 }
-
-void leerArchivo(char *nombre_archivo){
-
-    FILE *archivo;
-    archivo = fopen(nombre_archivo, "r");
-
-    if (archivo == NULL) {
-        perror("ERROR: The file is not opened.");
-                         }
-
-    char buffer[1024];
-    int j = 0;
-
-    while(fgets(buffer, 1024, archivo)){
-
-        int i = 0;
-        char *token = strtok(buffer, ",\n\r"); //Delimitar tokens por ',' '\n' y '\r'(windows)
-
-        while(token != NULL){
-
-            if(i == 0){
-            //printf("%.3s\t| ", token);
-            token = strtok(NULL, ",\n\r");
-            }
-
-            else if ((i+1)%2 == 0){
-            strcpy(materias[j][i/2], token);
-
-            //printf("%s ", token);
-            token = strtok(NULL, ",\n\r");
-            }
-
-            else if((i+1)%2 != 0){
-            strcpy(hora[(i/2)-1], token);
-
-            //printf("%s\t| ", token);
-            token = strtok(NULL, ",\n\r");
-            }
-                    i++;
-                            }
-        j++;
-                                        }
-
-
-    fclose(archivo);
-
-    formatearArchivo();
-}
-
-/*int main()
-{
-    leerArchivo("temps.txt");
-    formatearArchivo();
-
-    for(int i = 0; i < 7; i++){
-            for(int j = 0; j < ARRAY_COLS(materias); j++){
-                printf("Fila[%d] columna[%d]: %s\n",i , j , materias[j][i]);
-                                                         }
-            printf("\n");
-                                   }
-
-    return 0;
-}*/
--- main.c
+++ main.c
@@ -1,80 +1,48 @@
 #include <stdio.h>
-#include <string.h>
 #define ARRAY_LENGTH(x) (sizeof(x)/sizeof((x)[0]))
+#define ARRAY_COLS(x) (sizeof(x)[0]/sizeof((x)[0][0]))
 
 void preguntarHorario(int hora[], int minutos[], size_t);
-void formatearArchivo();
-void leerArchivo(char *nombre_archivo);
+void formatearArchivo(const char *nombre_arch, const char materias[7][12][50], const char hora[7][20], size_t tam_mat);
+
+const char* nombre_archivo= "horarios.csv";
 
 int main()
 {
-    char materias[50];
-    int hora[10];
-    int minutos[10];
+    char materias[7][12][50] = {0};
+    char hora[7][20];
     int clases;
-
-    FILE *temps;
 
     const char *dias[] = {
     "Lunes", "Martes", "Miercoles", "Jueves",
     "Viernes", "Sabado", "Domingo"
     };
 
-    size_t cantHoras = ARRAY_LENGTH(hora);
+    size_t tam_mat = ARRAY_LENGTH(materias); //Tamaño de los arreglos
+    size_t tam_hora = ARRAY_LENGTH(hora);
+
     int counter = 0;
         do{
         printf("Introduzca la hora: ");
-        scanf("%d", &hora[counter]);
-
-        printf("Introduzca los minutos: ");
-        scanf("%d", &minutos[counter]);
+        scanf("%s", hora[counter]);
 
         counter++;
-        }while(counter < cantHoras);
+        }while(counter < tam_hora);
     counter = 0;
 
-    temps = fopen("temps.txt", "w");
-
-    if (temps == NULL) {
-        printf("ERROR: The file is not opened.");
-        return 1;
-                        }
-
-
-        //int cantHoras = ARRAY_LENGTH(hora);
+        //PROBLEMA AL ESCRIBIR MAS DE 7 (En la función formatear archivo)
         for(int i = 0; i < 5; i++){
 
-        printf("Introducir el número de clases correspondiente a el día %s: ", dias[i]);
-        scanf("%d", &clases);
-        fprintf(temps, "%s,", dias[i]);
-
-        //preguntarHorario(hora, minutos, cantHoras);
+            printf("Introducir el número de clases correspondiente a el día %s: ", dias[i]);
+            scanf("%d", &clases);
 
                 for(int j = 0; j < clases; j++){
                         printf("Introduzca la asignatura: ");
-                        scanf("%s", &materias[0]);
-                        fprintf(temps, "%s,%d : %d,", &materias[0], hora[j], minutos[j]);
+                        scanf("%s", materias[i][j]);
                                                }
-                fprintf(temps, "\n");
                                   }
-    fclose(temps);
 
-    leerArchivo("temps.txt");
+    formatearArchivo(nombre_archivo, materias, hora, tam_mat);
 
     return 0;
 }
-
-void preguntarHorario(int hora[], int minutos[], size_t cantHoras){
-
-    char op1, op2;
-
-    for(int i = 0; i < cantHoras; i++){
-        printf("Desde %d : %d = %d\n", hora[i], minutos[i], i);
-                                      }
-        scanf(" %c", &op1);
-    for(int i = 0; i < cantHoras; i++){
-        printf("Hasta %d : %d = %d\n", hora[i], minutos[i], i);
-                                      }
-        scanf(" %c", &op2);
-}
-
